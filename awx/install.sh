dbeba3bdf1f7c897a666aef8f9ddb49e62f24731
Joe Berry <JBerry@advisor360.com>
2023-04-16 23:54:56 -0400

    guide

--- awx/install.sh
+++ awx/install.sh
@@ -1,5 +1,11 @@
 #install kustomize
+# cd ~/Public/local_machine_config_scripts/Linux
+# . install-kustomize.sh
+
 #run terraform to create aks cluster
+# cd ~/Public/terraform/aks_cluster
+#terraform apply
+
 #connect to aks cluster 
 #e.g. az aks get-credentials --resource-group rg-subtle-zebra --name k8stest
 kustomize build . | kubectl apply -f -
