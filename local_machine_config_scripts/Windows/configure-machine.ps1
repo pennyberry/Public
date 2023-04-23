5b0b4d9a98dea8e19d7baef070324eb6145738d8
Joe Berry <JBerry@advisor360.com>
2023-04-22 20:28:15 -0400

    it was wait...

--- local_machine_config_scripts/Windows/configure-machine.ps1
+++ local_machine_config_scripts/Windows/configure-machine.ps1
@@ -3,5 +3,5 @@ Invoke-WebRequest -Uri https://download.sysinternals.com/files/RDCMan.zip -OutFi
 Expand-Archive -Path c:\temp\RDCMan.zip -destination c:\temp\RDCMan -force
 Move-Item c:\temp\RDCMan\RDCMan.exe -Destination 'C:\Users\Public\Desktop\Remote Desktop Connection Manager.exe' -Force
 $ProgressPreference = 'SilentlyContinue' ; Invoke-WebRequest -Uri 'https://code.visualstudio.com/sha/download?build=stable&os=win32-x64' -OutFile c:\temp\vscode.exe
-Start-Process 'C:\temp\vscode.exe' -ArgumentList '/silent /norestart' -wait
+Start-Process 'C:\temp\vscode.exe' -ArgumentList '/silent /norestart'
 stop-transcript
