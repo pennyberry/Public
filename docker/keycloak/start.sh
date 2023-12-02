2348f3952dc1792c5946ebfb9e450bffcf716be6
Joe Berry <JBerry@advisor360.com>
2023-12-02 16:17:49 -0500

    fixed location of pg db

--- docker/keycloak/start.sh
+++ docker/keycloak/start.sh
@@ -1,4 +1,3 @@
-source env-vars.env
-docker-compose up -d
+docker-compose --env-file /home/joe/Public/docker/keycloak/env-vars.env -f /home/joe/Public/docker/keycloak/docker-compose.yml up -d
 sudo cp /home/joe/$domainname/nginx.conf /etc/nginx/sites-enabled/nginx.conf
 sudo service nginx restart
