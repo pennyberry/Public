5b8f26928f835f92190f280b587f92a9c5aabb35
Joe Berry <JBerry@advisor360.com>
2023-12-02 20:00:45 -0500

    no need for env vars here

--- docker/windows/startup-application.ps1
+++ docker/windows/startup-application.ps1
@@ -1,11 +1,7 @@
 #wait
 write-host waiting for boot up of wsl
 Wait-Event -Timeout 45
-#get env vars
-wsl cat /home/joe/Public/docker/lemmy/wsl/env-vars.env | foreach {
-    $name, $value = $_.split('=')
-    set-content env:\$name $value
-}
+
 #port forward to wsl
 $wsl_ip = (wsl hostname -I).trim()
 netsh interface portproxy delete v4tov4 listenport=443 listenaddress=0.0.0.0
