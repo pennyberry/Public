dd5300dd0d328579e9ec097fb1029199280a1cbf
Joe Berry <JBerry@advisor360.com>
2023-12-05 19:52:23 -0500

    doesn't need that much stuff

--- docker/nginx/configure-nginx.sh
+++ docker/nginx/configure-nginx.sh
@@ -1,9 +1,5 @@
-sudo docker-compose up -d
-sleep 5
 sudo apt install nginx
 #wget https://raw.githubusercontent.com/LemmyNet/lemmy-ansible/main/templates/nginx.conf
 #edit this file
 sudo cp /home/joe/Public/docker/nginx/nginx.conf /etc/nginx/sites-enabled/nginx.conf
-sudo nginx -s reload
 sudo service nginx restart
-#if running WSL remember to port forward in windows CMD
