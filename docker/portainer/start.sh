9fe707da5c430db5fa9831c25989e81c82657c74
Joe Berry <JBerry@advisor360.com>
2024-04-12 11:28:26 -0400

    portainer admin pw fix

--- docker/portainer/start.sh
+++ docker/portainer/start.sh
@@ -1,3 +1,5 @@
+#sudo rm -rf /home/joe/Public/docker/portainer/volumes
 source env-vars.env
-echo $adminpassword > /home/joe/Public/docker/portainer/portainer_password.secrets
+export PORTAINERADMINPASSWORD="$(docker run --rm httpd:2.4-alpine htpasswd -nbB admin $adminpassword | cut -d ":" -f 2)"
+#docker-compose --env-file /home/joe/Public/docker/portainer/env-vars.env -f /home/joe/Public/docker/portainer/docker-compose.yml down
 docker-compose --env-file /home/joe/Public/docker/portainer/env-vars.env -f /home/joe/Public/docker/portainer/docker-compose.yml up -d
--- docker/portainer/stop.sh
+++ docker/portainer/stop.sh
@@ -1,1 +1,4 @@
+sudo rm -rf /home/joe/Public/docker/portainer/volumes
+source env-vars.env
+export PORTAINERADMINPASSWORD="$(docker run --rm httpd:2.4-alpine htpasswd -nbB admin $adminpassword | cut -d ":" -f 2)"
 docker-compose --env-file /home/joe/Public/docker/portainer/env-vars.env -f /home/joe/Public/docker/portainer/docker-compose.yml down
