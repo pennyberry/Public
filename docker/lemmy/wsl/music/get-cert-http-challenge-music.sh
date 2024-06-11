a261298cb2a82b236c6498b9b63f3aaf342f1347
Joe Berry <JBerry@advisor360.com>
2024-06-10 20:58:53 -0400

    music.joeberry.org

---
+++ docker/lemmy/wsl/music/get-cert-http-challenge-music.sh
@@ -0,0 +1,18 @@
+source /home/joe/Public/docker/lemmy/wsl/music/env-vars.env
+sudo apt install certbot
+now=$(date +'%m-%d-%Y--%H-%M')
+domain=${domainname}
+logdir=/var/log/${domain}
+logname="${now}.log"
+log="${logdir}/${logname}"
+sudo mkdir ${logdir}
+sudo mkdir -p /var/www/certbot
+sudo chmod -R 755 /var/www/certbot
+sudo touch ${log}
+sudo chmod +x ${log}
+sudo chown joe:joe ${log}
+sudo chmod 755 ${logdir}
+sudo chmod 644 ${log}
+#sudo certbot certonly --manual --manual-public-ip-logging-ok --preferred-challenges http -d ${domain} >> ${log} 2>&1
+sudo certbot certonly --webroot -w /var/www/certbot -d ${domain} >> ${log} 2>&1
+sudo cat ${log}
