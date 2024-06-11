e69c8bbfdbc26085fba77e6b7e8e83f2e1125ab6
Joe Berry <JBerry@advisor360.com>
2024-06-10 21:01:02 -0400

    fixing cron job

--- docker/lemmy/wsl/music/configure-cert-cron-job.sh
+++ docker/lemmy/wsl/music/configure-cert-cron-job.sh
@@ -1,2 +1,2 @@
-sudo cp /home/joe/Public/docker/lemmy/wsl/get-cert-http-challenge-music.sh /etc/cron.weekly/get-cert-http-challenge-music.sh
+sudo cp /home/joe/Public/docker/lemmy/wsl/music/get-cert-http-challenge-music.sh /etc/cron.weekly/get-cert-http-challenge-music.sh
 sudo chmod +x /etc/cron.weekly/get-cert-http-challenge-music.sh
