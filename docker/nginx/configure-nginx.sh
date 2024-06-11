c4cb2350e234bc9d8ddb3e8e981064936d3a4706
Joe Berry <JBerry@advisor360.com>
2024-06-10 20:11:53 -0400

    music site

--- docker/nginx/configure-nginx.sh
+++ docker/nginx/configure-nginx.sh
@@ -2,4 +2,6 @@ sudo apt install nginx
 #wget https://raw.githubusercontent.com/LemmyNet/lemmy-ansible/main/templates/nginx.conf
 #edit this file
 sudo cp /home/joe/Public/docker/nginx/nginx.conf /etc/nginx/sites-enabled/nginx.conf
+sudo rm /var/www/html/index.nginx-debian.html
+sudo cp /home/joe/Public/docker/nginx/index.html /var/www/html/index.html
 sudo service nginx restart
--- docker/windows/startup-application.ps1
+++ docker/windows/startup-application.ps1
@@ -54,11 +54,24 @@ netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=443 connec
 netsh interface portproxy add v4tov4 listenaddress=0.0.0.0 listenport=80 connectaddress=$wsl_ip connectport=80
 
 #startup app inside wsl
-wsl bash -c ('echo 1 | sudo -S chmod +x /home/joe/Public/docker/keycloak/start.sh')
+#wsl bash -c ('echo 1 | sudo -S chmod +x /home/joe/Public/docker/keycloak/start.sh')
 wsl bash -c ('echo 1 | sudo -S chmod +x /home/joe/Public/docker/portainer/start.sh')
+write-host starting portainer
 wsl bash -c ('cd /home/joe/Public/docker/portainer ;. start.sh')
-wsl bash -c ('cd /home/joe/Public/docker/keycloak ;. start.sh')
+write-host starting keycloak
+#wsl bash -c ('cd /home/joe/Public/docker/keycloak ;. start.sh')
+write-host starting lemmy
 wsl.exe docker-compose --env-file /home/joe/Public/docker/lemmy/env-vars.env -f /home/joe/Public/docker/lemmy/docker-compose.yml up -d
+write-host restarting nginx
 wsl bash -c ('sudo -S service nginx restart <<< 1' -replace '"', '\"')
+
+#fixing broken containers
+write-host fixing broken containersx
+
+$containers = docker ps -a --filter "exited=127" --format "{{.ID}}"
+$containers
+foreach ($container_id in $containers) {
+    docker restart $container_id
+}
 
 Stop-Transcript
