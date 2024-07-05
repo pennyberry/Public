9fd9bb12d5c20d64baef572000fa4fcb43d78951
Joe Berry <JBerry@advisor360.com>
2024-07-05 11:53:06 -0400

    handling this multi folder problem

--- docker/lemmy/wsl/social/get-cert-http-challenge-social.sh
+++ docker/lemmy/wsl/social/get-cert-http-challenge-social.sh
@@ -18,15 +18,20 @@ sudo certbot certonly --webroot -w /var/www/certbot -d ${domain} >> ${log} 2>&1
 sudo cat ${log}
 
 # Move the certificate files to the correct directory
-# sudo mv /etc/letsencrypt/live/social.joeberry.org-0004/fullchain.pem /etc/letsencrypt/live/social.joeberry.org/fullchain.pem
-# sudo mv /etc/letsencrypt/live/social.joeberry.org-0004/privkey.pem /etc/letsencrypt/live/social.joeberry.org/privkey.pem
-
-# # Optionally, you can also move other related files if necessary
-# sudo mv /etc/letsencrypt/live/social.joeberry.org-0004/chain.pem /etc/letsencrypt/live/social.joeberry.org/chain.pem
-# sudo mv /etc/letsencrypt/live/social.joeberry.org-0004/cert.pem /etc/letsencrypt/live/social.joeberry.org/cert.pem
-
-# # Update permissions if needed
-# sudo chmod 600 /etc/letsencrypt/live/social.joeberry.org/fullchain.pem /etc/letsencrypt/live/social.joeberry.org/privkey.pem
-
-# # Reload or restart Nginx to apply the changes
-# sudo systemctl reload nginx
+# sudo mv /etc/letsencrypt/live/social.joeberry.org-0005/fullchain.pem /etc/letsencrypt/live/social.joeberry.org/fullchain.pem
+# Find folders that keep appending 0004, 0005, etc.
+folder=$(sudo find /etc/letsencrypt/live -type d -name "*-[0-9][0-9][0-9][0-9]" | head -n 1)
+if [ -d "$folder" ]; then
+    # Move the certificate files to the correct directory
+    sudo mv "$folder/fullchain.pem" "/etc/letsencrypt/live/social.joeberry.org/fullchain.pem"
+    sudo mv "$folder/chain.pem" "/etc/letsencrypt/live/social.joeberry.org/chain.pem"
+    sudo mv "$folder/cert.pem" "/etc/letsencrypt/live/social.joeberry.org/cert.pem"
+    sudo mv "$folder/privkey.pem" "/etc/letsencrypt/live/social.joeberry.org/privkey.pem"
+    # Update permissions if needed
+    sudo chmod 600 /etc/letsencrypt/live/social.joeberry.org/fullchain.pem /etc/letsencrypt/live/social.joeberry.org/privkey.pem
+    sudo rm -rf "$folder"
+    # Reload or restart Nginx to apply the changes
+    sudo systemctl reload nginx
+else
+    echo "folder not found. no action required"
+fi
