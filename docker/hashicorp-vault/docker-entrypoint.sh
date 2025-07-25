2c8b49a1cc5ceff4f57da39329b3816c514ee03d
Joe Berry <pennyberry1@gmail.com>
2025-07-25 14:12:32 +0000

    environment variables for vault and OIDC

--- docker/hashicorp-vault/docker-entrypoint.sh
+++ docker/hashicorp-vault/docker-entrypoint.sh
@@ -6,8 +6,8 @@ exec /usr/local/bin/docker-entrypoint.sh $@ &
 VAULT_PID=$!
 
 # Wait for Vault to finalize start up.
-export VAULT_ADDR="${VAULT_API_ADDR:-http://127.0.0.1:8200}"
-export VAULT_TOKEN="${VAULT_DEV_ROOT_TOKEN_ID:-root}"
+export VAULT_ADDR="${VAULT_API_ADDR}"
+export VAULT_TOKEN="${VAULT_DEV_ROOT_TOKEN_ID}"
 until vault status >/dev/null 2>&1; do
   sleep 1
 done
@@ -21,6 +21,19 @@ if [ -d "/docker-entrypoint-initvault.d" ]; then
     fi
   done
 fi
+
+vault auth enable oidc
+
+vault write auth/oidc/config \
+    oidc_discovery_url="${VAULT_OIDC_DISCOVERY_URL}" \
+    oidc_client_id=${VAULT_OIDC_CLIENT_ID} \
+    oidc_client_secret=${VAULT_OIDC_CLIENT_SECRET} \
+    default_role=default \
+
+vault write auth/oidc/role/default \
+    user_claim=email \
+    oidc_scopes=email \
+    allowed_redirect_uris=${VAULT_OIDC_REDIRECT_URI}
 
 echo "Vault initialized"
 wait $VAULT_PID
