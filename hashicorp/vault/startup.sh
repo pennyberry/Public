03be00d2680ed583209473498ef6f83103841d16
Joe Berry <pennyberry1@gmail.com>
2025-07-30 13:46:30 +0000

    this is a duplicate

--- hashicorp/vault/startup.sh
+++
@@ -1,7 +0,0 @@
-source /home/joe/Public/hashicorp/vault/env-vars.env
-export VAULT_ADDR=https://${domainname}:8200
-export VAULT_CACERT="/opt/vault/tls/tls.crt"
-vault operator unseal $(grep "Unseal Key" /home/joe/Public/hashicorp/vault/key.secrets | awk 'NR==1 {print $NF}')
-vault operator unseal $(grep "Unseal Key" /home/joe/Public/hashicorp/vault/key.secrets | awk 'NR==2 {print $NF}')
-vault operator unseal $(grep "Unseal Key" /home/joe/Public/hashicorp/vault/key.secrets | awk 'NR==3 {print $NF}')
-vault login $(grep "Initial Root Token" /home/joe/Public/hashicorp/vault/key.secrets | awk 'NR==1 {print $NF}')
