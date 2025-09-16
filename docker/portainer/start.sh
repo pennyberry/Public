72f33c2fdb5b275e8c6a42fe3b8a3593b290e9b7
Joe Berry <pennyberry1@gmail.com>
2025-09-16 22:40:41 +0000

    fixing portainer start commands

--- docker/portainer/start.sh
+++ docker/portainer/start.sh
@@ -2,4 +2,4 @@
 source /home/joe/Public/docker/portainer/env-vars.env
 export PORTAINERADMINPASSWORD="$(docker run --rm httpd:2.4-alpine htpasswd -nbB admin $adminpassword | cut -d ":" -f 2)"
 #docker-compose --env-file /home/joe/Public/docker/portainer/env-vars.env -f /home/joe/Public/docker/portainer/docker-compose.yml down
-docker-compose --env-file /home/joe/Public/docker/portainer/env-vars.env -f /home/joe/Public/docker/portainer/docker-compose.yml up -d
+docker compose --env-file /home/joe/Public/docker/portainer/env-vars.env -f /home/joe/Public/docker/portainer/docker-compose.yml up -d
