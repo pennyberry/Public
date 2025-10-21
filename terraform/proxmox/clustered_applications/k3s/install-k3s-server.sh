f28cb40f7dc5a242cb5557d07f3fa1508e8596b4
Joe Berry <pennyberry1@gmail.com>
2025-10-21 18:42:55 -0400

    comments

--- terraform/proxmox/clustered_applications/k3s/install-k3s-server.sh
+++ terraform/proxmox/clustered_applications/k3s/install-k3s-server.sh
@@ -1,3 +1,4 @@
+#install k3s server - we're not using the default servicelb, we're using MetalLB instead
 sudo curl -sfL https://get.k3s.io | sh -s - --token k3s_token --disable=traefik,servicelb
 sudo ufw allow 6443/tcp #apiserver
 sudo ufw allow from 10.42.0.0/16 to any #pods
@@ -9,6 +10,7 @@ sudo kubectl apply -f https://raw.githubusercontent.com/metallb/metallb/v0.15.2/
 #wait for deployment to complete
 sudo kubectl rollout status deployment -n metallb-system
 
+# Apply MetalLB configuration with the dedicated external IP - make sure you set k3s_cluster_ip variable in your terraform code / env variables!
 sudo kubectl apply -f - <<'EOF'
 apiVersion: metallb.io/v1beta1
 kind: IPAddressPool
