df5ff980da0df5ae843cf308453b062b9805d72d
Joe Berry <pennyberry1@gmail.com>
2025-10-22 16:46:22 -0400

    k3s upgrades

---
+++ kubectl/k3s-cluster-upgrade/setup-upgrades.sh
@@ -0,0 +1,18 @@
+#configure automatic upgrades for the cluster
+sudo kubectl apply -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/crd.yaml -f https://github.com/rancher/system-upgrade-controller/releases/latest/download/system-upgrade-controller.yaml
+
+#wait for deployment to complete
+sudo kubectl rollout status deployment -n system-upgrade
+
+#create upgrade plans for server and agent nodes
+sudo kubectl apply -f ./deployment.yaml
+
+#note this is a hardcoded version. at the time of testing, the channel method was not working as expected.
+#you can replace version with channel if you want to track a channel instead of a specific version... if it ever works...
+#channel: https://update.k3s.io/v1-release/channels/stable
+
+#to check on updates, some commands you can run
+#kubectl describe plan -n system-upgrade server-plan
+#kubectl -n system-upgrade logs deployment.apps/system-upgrade-controller
+#kubectl get plan -n system-upgrade -o wide
+#kubectl -n system-upgrade describe jobs
