9e0fc3be9a77541179c1610d48ca80b134ec1ff1
Joe Berry <pennyberry1@gmail.com>
2025-11-02 23:09:05 -0500

    added commands for expanding disks

--- local_machine_config_scripts/Linux/proxmox-expand-vm-disk-ubuntu.sh
+++ local_machine_config_scripts/Linux/proxmox-expand-vm-disk-ubuntu.sh
@@ -4,8 +4,12 @@
 # pvdisplay    
 # lvdisplay
 
-#expand the disk with
+#expand the disk that use Logical Volumes with:
 sudo growpart /dev/sda 3
 sudo pvresize /dev/sda3
 sudo lvextend -l +100%FREE /dev/ubuntu-vg/ubuntu-lv
 sudo resize2fs /dev/ubuntu-vg/ubuntu-lv
+
+#expand the disk using ext4 partitions:
+sudo growpart /dev/sda 1
+sudo resize2fs /dev/sda1
