f425c5eb488147c3b7729b2ef3bd08704e273d9d
Joe Berry <pennyberry1@gmail.com>
2025-12-22 14:05:25 -0500

    added annotations

--- kubectl/keel/configure-keel.ps1
+++ kubectl/keel/configure-keel.ps1
@@ -4,7 +4,13 @@ $pods | foreach {
 
   #do
   kubectl annotate deployment $_ -n $namespace keel.sh/policy=all
+  kubectl annotate deployment $_ -n $namespace keel.sh/policy=force
+  kubectl annotate deployment $_ -n $namespace keel.sh/trigger=poll      
+  kubectl annotate deployment $_ -n $namespace keel.sh/pollSchedule="@every 10m"
 
   #undo
   # kubectl annotate deployment $_ -n $namespace keel.sh/policy-
+  # kubectl annotate deployment $_ -n $namespace keel.sh/policy-
+  # kubectl annotate deployment $_ -n $namespace keel.sh/trigger-
+  # kubectl annotate deployment $_ -n $namespace keel.sh/pollSchedule-
 }
