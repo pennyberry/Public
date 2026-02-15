fe90d27aeca6f883aab5e7c5fe91aa8e09795e5b
Joe Berry <pennyberry1@gmail.com>
2026-02-15 12:40:32 -0500

    updated logging

--- powershell/patch-windows-iso/modules/create-iso.ps1
+++ powershell/patch-windows-iso/modules/create-iso.ps1
@@ -12,7 +12,7 @@ try {
     start-process -FilePath $oscdimg -ArgumentList "-m -o -u2 -udfver102 -bootdata:2#p0,e,b$($etfsboot.FullName)#pEF,e,b$($efisys.FullName) $($source.FullName) $nameofiso" -Wait -noNewWindow
     remove-item .\install.wim
     remove-item .\source -Recurse -Force
-    get-item .\$nameofiso | Move-Item -Destination ..\$nameofiso -Force
+    write-host "Saved patched iso to $((get-item .\*patched.iso).FullName)"
 }
 catch {
     Write-Host "Error creating ISO: $_.Exception.Message"
